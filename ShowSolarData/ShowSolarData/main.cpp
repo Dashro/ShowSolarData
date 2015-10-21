33c77d3527595ed30be329768ed7901bd8e12d44
Dashro <wirtzfabian@yahoo.de>
2015-10-21 15:42:40 +0200

    -bugfix

--- ShowSolarData/ShowSolarData/main.cpp
+++ ShowSolarData/ShowSolarData/main.cpp
@@ -14,9 +14,6 @@ int main(int argc, char *argv[])
 		arguments[i].remove("-");
 	}
 
-	arguments.append("ip");
-	arguments.append("127.0.0.1:80");
-
 	QCoreApplication::setOrganizationName("Fabian Production");
 	QCoreApplication::setApplicationName("ShowSolarData");
 
@@ -27,13 +24,13 @@ int main(int argc, char *argv[])
 	else
 		w.show();
 
-	//if(arguments.contains("ip"))
-	//{
-	//	w.onNewURL(arguments.at(arguments.indexOf("ip") + 1));
-	//	QSettings settings;
-	//	settings.setValue("IPAdress", arguments.at(arguments.indexOf("ip") + 1).split(":").first());
-	//	settings.setValue("Port", arguments.at(arguments.indexOf("ip") + 1).split(":").last());
-	//}
+	if(arguments.contains("ip"))
+	{
+		w.onNewURL(arguments.at(arguments.indexOf("ip") + 1));
+		QSettings settings;
+		settings.setValue("IPAdress", arguments.at(arguments.indexOf("ip") + 1).split(":").first());
+		settings.setValue("Port", arguments.at(arguments.indexOf("ip") + 1).split(":").last());
+	}
 
 	return a.exec();
 }
